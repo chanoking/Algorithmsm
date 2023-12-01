32df3d6dd89f0f222ca32eaa846bc1fd91c5e81c
진찬호 <chanhojin94@kakao.com>
2023-12-01 22:02:41 +0900

    Solving fizzBuzz with style

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,44 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// function maxChar(str) {
+//   let charMap = {};
+//   let maxCount = 0;
+//   let maxChar = "";
+//   for (let char of str) {
+//     if (!charMap[char]) {
+//       charMap[char] = 1;
+//     } else {
+//       charMap[char]++;
+//     }
+//     if (charMap[char] > maxCount) {
+//       maxCount = charMap[char];
+//       maxChar = char;
+//     }
+//   }
+//   return maxChar;
+// }
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,25 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = n.toString().split("").reverse().join("");
+
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//     let char = n.toString();
+//     let reversed = char.split("").reverse().join("");
+//     //   console.log(reversed);
+//     let answer = parseInt(reversed);
+//     //   console.log(answer);
+//     if (n > 0) {
+//       return answer;
+//     } else if (n === 0) {
+//       return 0;
+//     } else {
+//       return -answer;
+//     }
+//   }
