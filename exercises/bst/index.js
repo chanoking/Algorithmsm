208a2e210c228fe5c594546b9ed7cc70bc43b0c7
진찬호 <chanhojin94@kakao.com>
2023-12-07 00:30:44 +0900

    bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,43 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+
+  insert(data) {
+    if (data < this.data) {
+      // Insert data to the left subtree
+      if (!this.left) {
+        this.left = new Node(data);
+      } else {
+        this.left.insert(data);
+      }
+    } else if (data > this.data) {
+      // Insert data to the right subtree
+      if (!this.right) {
+        this.right = new Node(data);
+      } else {
+        this.right.insert(data);
+      }
+    }
+    // Ignore if data is equal to this.data (no duplicates in this example)
+  }
+
+  contains(data) {
+    if (data === this.data) {
+      return this; // Return the node itself
+    } else if (data < this.data && this.left) {
+      return this.left.contains(data);
+    } else if (data > this.data && this.right) {
+      return this.right.contains(data);
+    }
+
+    return null; // Return null if the vlaue is not found
+  }
+}
 
 module.exports = Node;
