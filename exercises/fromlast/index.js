56c213a31815968624f6389666639b8eb45efa5f
진찬호 <chanhojin94@kakao.com>
2023-12-06 13:29:24 +0900

    fromLast

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,40 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+// function fromLast(list, n) {
+//   let slow = list.getFirst();
+//   let fast = list.getFirst();
+
+//   // Move the fast pointer n nodes ahead
+//   for (let i = 0; i < n; i++) {
+//     fast = fast.next;
+//   }
+
+//   // Move both pointers until the fast pointer reaches the end
+//   while (fast.next) {
+//     fast = fast.next;
+//     slow = slow.next;
+//   }
+
+//   // slow now points to the node n spaces from the last
+//   return slow;
+// }
+
+function fromLast(list, n) {
+  let fast = list.getFirst();
+  let slow = list.getFirst();
+
+  while (n > 0) {
+    fast = fast.next;
+    n--;
+  }
+
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
+}
 
 module.exports = fromLast;
